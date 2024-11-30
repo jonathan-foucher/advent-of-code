563d7db13c39b19e7c33ce318872bcbe8ff3f41e
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-30 22:33:39 +0100

    2023 day 3

---
+++ 2023/day-03/main.js
@@ -0,0 +1,52 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const isNearSpecialChar = (row, col, charMatrix) => {
+  return row != 0 && charMatrix[row - 1].slice(Math.max(col - 1, 0), col + 2).some(char => char.isSpecChar)
+  || col != 0 && charMatrix[row][col - 1].isSpecChar
+  || charMatrix[row][col + 1] && charMatrix[row][col + 1].isSpecChar
+  || charMatrix[row + 1] && charMatrix[row + 1].slice(Math.max(col - 1, 0), col + 2).some(char => char.isSpecChar)
+}
+
+let row = -1
+let charMatrix = readFile(FILE_NAME)
+  .map(line => {
+    row++
+    let col = -1
+    return line.split('')
+      .map(char => {
+        col++
+        return { value: char, row, col, isNumber: !isNaN(char), isSpecChar: isNaN(char) && char != '.' }
+      })
+  })
+
+charMatrix = charMatrix.map(line => line.map(char => {
+    return { ...char, isActive: char.isNumber && isNearSpecialChar(char.row, char.col, charMatrix) }
+  }))
+
+const result = charMatrix.flatMap(line => {
+    const res = ['']
+    let isActive = false
+    line.forEach(char => {
+      if (char.isNumber) {
+        res[res.length - 1] += char.value
+        isActive = isActive || char.isActive
+      } else {
+        if (!isActive) {
+          res.pop()
+        } else {
+          isActive = false
+        }
+        res.push('')
+      }
+    })
+    if (!isActive) {
+      res.pop()
+    }
+    return res.filter(r => r)
+  })
+  .map(number => parseInt(number))
+  .reduce((acc, val) => acc + val, 0)
+
+console.log(result)
---
+++ 2023/day-03/main2.js
@@ -0,0 +1,59 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const getRowActiveNumbers = (line, col) => {
+  let res = ['']
+  let isActive = false
+  line.forEach(char => {
+    if (char.isNumber) {
+      res[res.length - 1] += char.value
+      isActive = isActive || col - 1 <= char.col && char.col <= col + 1
+    } else {
+      if (!isActive) {
+        res.pop()
+      } else {
+        isActive = false
+      }
+      res.push('')
+    }
+  })
+  if (!isActive) {
+    res.pop()
+  }
+  return res.filter(r => r)
+    .map(r => parseInt(r))
+}
+
+const getGearFactor = (row, col, charMatrix) => {
+  let activeNumbers = []
+  if (row != 0) {
+    activeNumbers.push(getRowActiveNumbers(charMatrix[row - 1], col))
+  }
+  activeNumbers.push(getRowActiveNumbers(charMatrix[row], col))
+  if (charMatrix[row + 1]) {
+    activeNumbers.push(getRowActiveNumbers(charMatrix[row + 1], col))
+  }
+  activeNumbers = activeNumbers.flatMap(r => r)
+  return activeNumbers.length === 2 ? activeNumbers[0] * activeNumbers[1] : 0
+}
+
+let row = -1
+let charMatrix = readFile(FILE_NAME)
+  .map(line => {
+    row++
+    let col = -1
+    return line.split('')
+      .map(char => {
+        col++
+        return { value: char, row, col, isNumber: !isNaN(char), isGear: char === '*' }
+      })
+  })
+
+const result = charMatrix.flatMap(line =>
+    line.filter(char => char.isGear)
+      .map(char => getGearFactor(char.row, char.col, charMatrix))
+  )
+  .reduce((acc, val) => acc + val, 0)
+
+console.log(result)
