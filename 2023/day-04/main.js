158e62667c3c1015a3d7a6543f05a2e039bda224
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-12-01 07:15:43 +0100

    2023 day 4

---
+++ 2023/day-04/main.js
@@ -0,0 +1,18 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const result = readFile(FILE_NAME)
+  .map(line => {
+    return {
+      id: line.split(':')[0],
+      winning_numbers: line.split(':')[1].split('|')[0].split(' ').filter(str => str).map(str => parseInt(str)),
+      numbers: line.split(':')[1].split('|')[1].split(' ').filter(str => str).map(str => parseInt(str))
+    }
+  })
+  .map(card => card.numbers.filter(n => card.winning_numbers.some(wn => wn === n)).length)
+  .filter(count => count != 0)
+  .map(count => Math.pow(2, count - 1))
+  .reduce((acc, val) => acc + val, 0)
+
+console.log(result)
---
+++ 2023/day-04/main2.js
@@ -0,0 +1,34 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const cards = readFile(FILE_NAME)
+  .map(line => {
+    const winningNumbers = line.split(':')[1].split('|')[0].split(' ').filter(str => str).map(str => parseInt(str))
+    const numbers = line.split(':')[1].split('|')[1].split(' ').filter(str => str).map(str => parseInt(str))
+    const matchCount = numbers.filter(n => winningNumbers.some(wn => wn === n)).length
+    return {
+      id: line.split(':')[0],
+      winning_numbers: winningNumbers,
+      numbers,
+      match_count: matchCount
+    }
+  })
+
+let multipliers = []
+
+const result = cards.flatMap((card, i) => {
+    const res = []
+    const multiplier = multipliers.length
+    multipliers = multipliers.map(n => n - 1).filter(n => n != 0)
+    for (i = 0; i <= multiplier; i++) {
+      res.push(card)
+      if (card.match_count > 0) {
+        multipliers.push(card.match_count)
+      }
+    }
+    return res
+  })
+  .length
+
+console.log(result)
