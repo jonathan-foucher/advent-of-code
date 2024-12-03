8324bccc3e2238af40743a9ae6f03ed43b8d496c
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-12-03 06:19:47 +0100

    2024 day 3

---
+++ 2024/day-03/main.js
@@ -0,0 +1,15 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const MUL_REGEX = /mul\([0-9]{1,3},[0-9]{1,3}\)/g
+
+const result = readFile(FILE_NAME)
+  .flatMap(line => line.match(MUL_REGEX))
+  .map(str => {
+    const res = str.replace('mul(', '').replace(')', '').split(',')
+    return parseInt(res[0]) * parseInt(res[1])
+  })
+  .reduce((acc, val) => acc + val, 0)
+
+console.log(result)
---
+++ 2024/day-03/main2.js
@@ -0,0 +1,17 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const MUL_REGEX = /mul\([0-9]{1,3},[0-9]{1,3}\)/g
+
+const result = readFile(FILE_NAME)
+  .join('').split('do()')
+  .flatMap(line => line.split('don\'t()')[0])
+  .flatMap(line => line.match(MUL_REGEX))
+  .map(str => {
+    const res = str.replace('mul(', '').replace(')', '').split(',')
+    return parseInt(res[0]) * parseInt(res[1])
+  })
+  .reduce((acc, val) => acc + val, 0)
+
+console.log(result)
