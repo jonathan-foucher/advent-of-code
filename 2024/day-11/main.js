53fb67335c040b1d35d0a6bb13617ebe8da0de61
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-12-11 09:24:51 +0100

    2024 day 11

---
+++ 2024/day-11/main.js
@@ -0,0 +1,24 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const updateStone = (value) => {
+  if (value === 0) {
+    return [ 1 ]
+  }
+
+  const strValue = "" + value
+  if (strValue.length % 2 === 0) {
+    const nDigits = strValue.length / 2
+    return [ parseInt(strValue.slice(0, nDigits)), parseInt(strValue.slice(nDigits)) ]
+  }
+  
+  return [ value * 2024 ]
+}
+
+let result = readFile(FILE_NAME)[0].split(' ').map(e => parseInt(e))
+for (let i = 0; i < 25; i++) {
+  result = result.flatMap(stone => updateStone(stone))
+}
+
+console.log(result.length)
---
+++ 2024/day-11/main2.js
@@ -0,0 +1,42 @@
+import { readFile } from './file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const countLimit = 75
+const cache = new Map()
+
+const blink = (value, counter) => {
+  if (counter === countLimit) {
+    return 1
+  }
+  const key = `${value}-${counter}`
+  const cachedValue = cache.get(key)
+  if (cachedValue) {
+    return cachedValue
+  } else {
+    let res
+    if (value === 0) {
+      res = blink(1, counter + 1)
+    } else {
+      const strValue = value.toString()
+      const strLength = strValue.length
+      if (strLength % 2 === 0) {
+        const nDigits = strLength / 2
+        res = blink(parseInt(strValue.slice(0, nDigits)), counter + 1) + blink(parseInt(strValue.slice(nDigits)), counter + 1)
+      } else {
+        res = blink(value * 2024, counter + 1)
+      }
+    }
+    cache.set(key, res)
+    return res
+  }
+}
+
+let numbers = readFile(FILE_NAME)[0].split(' ').map(e => parseInt(e))
+let result = 0
+
+for (let j = 0; j < numbers.length; j++) {
+  result += blink(numbers[j], 0)
+}
+
+console.log(result)
