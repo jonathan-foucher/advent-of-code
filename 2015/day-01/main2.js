e93abb1202ddff1085f8e7806308fcdf189918dd
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-12-25 12:37:36 +0100

    2015 day 1

---
+++ 2015/day-01/main2.js
@@ -0,0 +1,18 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const inputs = readFile(FILE_NAME)[0].split('')
+
+let floor = 0
+let i = 0
+while (i < inputs.length && floor >= 0) {
+  if (inputs[i] === '(') {
+    floor++
+  } else {
+    floor--
+  }
+  i++
+}
+
+console.log(i)
