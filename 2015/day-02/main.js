ecbf5920dc86906e121def1625b79097ac85c95a
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-12-25 13:00:06 +0100

    2015 day 2

---
+++ 2015/day-02/main.js
@@ -0,0 +1,25 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const dimensions = readFile(FILE_NAME).map(line =>
+  line.split('x').map(value => parseInt(value))
+    .sort((a, b) => {
+      return a - b
+    })
+)
+
+let result = 0
+
+for (let i = 0; i < dimensions.length; i++) {
+  const dimension = dimensions[i]
+  for (let j = 0; j < dimension.length - 1; j++) {
+    for (let k = 1; k < dimension.length; k++)
+      if (k !== j ) {
+        result += 2 * dimension[j] * dimension[k]
+      }
+  }
+  result += dimension[0] * dimension[1]
+}
+
+console.log(result)
---
+++ 2015/day-02/main2.js
@@ -0,0 +1,23 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const dimensions = readFile(FILE_NAME).map(line =>
+  line.split('x').map(value => parseInt(value))
+    .sort((a, b) => {
+      return a - b
+    })
+)
+
+let result = 0
+
+for (let i = 0; i < dimensions.length; i++) {
+  const dimension = dimensions[i]
+  let mult = 1
+  for (let j = 0; j < dimension.length; j++) {
+    mult = mult * dimension[j]
+  }
+  result += mult + 2 * (dimension[0] + dimension[1])
+}
+
+console.log(result)
