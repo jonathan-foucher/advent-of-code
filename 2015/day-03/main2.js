ece68c9cb7a453d0fb6b8f94b87c13c19324d1c7
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-12-25 13:14:13 +0100

    2015 day 3

---
+++ 2015/day-03/main2.js
@@ -0,0 +1,52 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const directions = readFile(FILE_NAME)[0].split('')
+
+let x1 = 0
+let y1 = 0
+let x2 = 0
+let y2 = 0
+const visitedLocations = [ '0-0' ]
+
+for (let i = 0; i < directions.length; i++) {
+  const direction = directions[i]
+  switch (direction) {
+    case '>':
+      if (i % 2 === 0) {
+        x1++
+      } else {
+        x2++
+      }
+      break
+    case '<':
+      if (i % 2 === 0) {
+        x1--
+      } else {
+        x2--
+      }
+      break
+    case '^':
+      if (i % 2 === 0) {
+        y1--
+      } else {
+        y2--
+      }
+      break
+    case 'v':
+      if (i % 2 === 0) {
+        y1++
+      } else {
+        y2++
+      }
+      break
+  }
+  
+  const key = i % 2 === 0 ? `${x1}-${y1}` : `${x2}-${y2}`
+  if (!visitedLocations.includes(key)) {
+    visitedLocations.push(key)
+  }
+}
+
+console.log(visitedLocations.length)
