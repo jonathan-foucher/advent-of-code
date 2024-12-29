e49e762501ff0593a98b84889db3a13363e3c574
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-12-29 20:42:51 +0100

    add bitwise operations for numbers > 31 bits

--- utils/javascript/math-utils.js
+++ utils/javascript/math-utils.js
@@ -1,8 +1,32 @@
+const shift = 2 ** 31
+
 export const xor = (n2, n1) => {
-  const shift = 2 ** 31
   const n1h = Math.trunc(n1 / shift)
   const n2h = Math.trunc(n2 / shift)
   const n1l = n1 % shift
   const n2l = n2 % shift
   return shift * new Number(n1h ^ n2h) + new Number(n1l ^ n2l)
+}
+
+export const and = (n2, n1) => {
+  const n1h = Math.trunc(n1 / shift)
+  const n2h = Math.trunc(n2 / shift)
+  const n1l = n1 % shift
+  const n2l = n2 % shift
+  return shift * new Number(n1h & n2h) + new Number(n1l & n2l)
+}
+
+export const or = (n2, n1) => {
+  const n1h = Math.trunc(n1 / shift)
+  const n2h = Math.trunc(n2 / shift)
+  const n1l = n1 % shift
+  const n2l = n2 % shift
+  return shift * new Number(n1h | n2h) + new Number(n1l | n2l)
+}
+
+export const not = (n, nbBits = 0) => {
+  return parseInt(n.toString(2).padStart(nbBits, '0')
+    .split('')
+    .map(c => c === '1' ? 0 : 1)
+    .join(''), 2)
 }
