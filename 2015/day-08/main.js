bb79e5380e75cb5c5c588c7fd5a2797e9cc42eb7
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-12-31 00:19:03 +0100

    2015 day 8

---
+++ 2015/day-08/main.js
@@ -0,0 +1,12 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const strArray = readFile(FILE_NAME)
+  
+let result = 0
+for (let i = 0; i < strArray.length; i++) {
+  result += strArray[i].length - strArray[i].replaceAll(/\\(x[0-9a-f]{2}|\\|\")/gi, '_').length + 2
+}
+
+console.log(result)
