189fc80ed38034b027439665bcbdcfbbbe956d63
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2024-12-31 18:22:33 +0100

    2015 day 10

---
+++ 2015/day-10/main.js
@@ -0,0 +1,27 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const NB_TURNS = 50
+
+let str = readFile(FILE_NAME)[0]
+
+for (let i = 0; i < NB_TURNS; i++) {
+  let result = ''
+  let prevChar
+  let count = 0
+  
+  for (let j = 0; j <= str.length; j++) {
+    const currentChar = str[j]
+    if (j > 0 && currentChar !== prevChar) {
+      result += count + prevChar
+      count = 0
+    }
+    prevChar = currentChar
+    count++
+  }
+  
+  str = result
+}
+
+console.log(str.length)
