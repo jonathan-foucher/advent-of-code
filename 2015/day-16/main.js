f840963d5095c1f86fbb38442a1893945fe10a9c
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-01 19:56:17 +0100

    2015 day 16

---
+++ 2015/day-16/main.js
@@ -0,0 +1,45 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const TARGET = {
+  'children': 3,
+  'cats': 7,
+  'samoyeds': 2,
+  'pomeranians': 3,
+  'akitas': 0,
+  'vizslas': 0,
+  'goldfish': 5,
+  'trees': 3,
+  'cars': 2,
+  'perfumes': 1
+}
+
+const aunts = readFile(FILE_NAME).map(line => {
+  const id = parseInt(/(?<=^Sue )\d+(?=:)/.exec(line)[0])
+  const list = /(?<=^Sue \d+: ).*/.exec(line)[0]
+    .split(', ')
+    .map(element => {
+      return { key: element.split(': ')[0], value: parseInt(element.split(': ')[1]) }
+    })
+  return { id, list }
+})
+
+let result
+for (let i = 0; i < aunts.length; i++) {
+  const aunt = aunts[i]
+  let isPossible = true
+  for (let j = 0; j < aunt.list.length; j++) {
+    const element = aunt.list[j]
+    if (TARGET[element.key] !== element.value) {
+      isPossible = false
+      break
+    }
+  }
+  if (isPossible) {
+    result = aunt.id
+    break
+  }
+}
+
+console.log(result)
