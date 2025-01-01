5bdf31a3e5b5414f898692721d8c69d51db46429
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-01 15:16:33 +0100

    2015 day 14

---
+++ 2015/day-14/main.js
@@ -0,0 +1,26 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const TIME_LIMIT = 2503
+
+const reindeers = readFile(FILE_NAME).map(line => {
+  const name = /^[a-zA-Z]+(?= )/.exec(line)[0]
+  const speed = parseInt(/(?<= can fly )\d+(?= km\/s)/.exec(line)[0])
+  const flyingTime = parseInt(/(?<= can fly \d+ km\/s for )\d+(?= seconds)/.exec(line)[0])
+  const restingTime = parseInt(/(?<= must rest for )\d+(?= seconds)/.exec(line)[0])
+  return { name, speed, flyingTime, restingTime }
+})
+
+let result = 0
+for (let i = 0; i < reindeers.length; i++) {
+  const reindeer = reindeers[i]
+  const distance = Math.floor(TIME_LIMIT / (reindeer.flyingTime + reindeer.restingTime)) * reindeer.speed * reindeer.flyingTime
+    + Math.min(TIME_LIMIT % (reindeer.flyingTime + reindeer.restingTime), reindeer.flyingTime) * reindeer.speed
+
+  if (distance > result) {
+    result = distance
+  }
+}
+
+console.log(result)
---
+++ 2015/day-14/main2.js
@@ -0,0 +1,42 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const TIME_LIMIT = 2503
+
+const reindeers = readFile(FILE_NAME).map(line => {
+  const name = /^[a-zA-Z]+(?= )/.exec(line)[0]
+  const speed = parseInt(/(?<= can fly )\d+(?= km\/s)/.exec(line)[0])
+  const flyingTime = parseInt(/(?<= can fly \d+ km\/s for )\d+(?= seconds)/.exec(line)[0])
+  const restingTime = parseInt(/(?<= must rest for )\d+(?= seconds)/.exec(line)[0])
+  return {
+    name, speed, flyingTime, restingTime,
+    score: 0, distance: 0, flyingTimeLeft: flyingTime, restingTimeLeft: restingTime
+  }
+})
+
+for (let _ = 0; _ < TIME_LIMIT; _++) {
+  for (let i = 0; i < reindeers.length; i++) {
+    const reindeer = reindeers[i]
+    if (reindeer.flyingTimeLeft > 0) {
+      reindeer.distance += reindeer.speed
+      reindeer.flyingTimeLeft--
+      continue
+    }
+    if (reindeer.restingTimeLeft > 0) {
+      reindeer.restingTimeLeft--
+      if (reindeer.restingTimeLeft === 0) {
+        reindeer.flyingTimeLeft = reindeer.flyingTime
+        reindeer.restingTimeLeft = reindeer.restingTime
+      }
+    }
+  }
+
+  let maxDistance = Math.max(...reindeers.map(reindeer => reindeer.distance))
+  reindeers.filter(reindeer => reindeer.distance === maxDistance)
+    .forEach(reindeer => reindeer.score++)
+}
+
+let result = Math.max(...reindeers.map(reindeer => reindeer.score))
+
+console.log(result)
