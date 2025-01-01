17ff1f22869656e50e6e36cb6870d957999905b8
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-01 11:58:29 +0100

    2015 day 12

---
+++ 2015/day-12/main2.js
@@ -0,0 +1,32 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt' 
+
+const input = readFile(FILE_NAME)[0]
+
+const getSum = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map(value => getSum(value))
+      .filter(element => Number.isInteger((element)))
+      .reduce((acc, val) => acc + val, 0)
+  }
+  
+  if (Object.values(obj).includes('red')) {
+    return 0
+  }
+  
+  if (Number.isInteger(obj)) {
+    return obj
+  }
+  
+  if (typeof obj === 'string') {
+    return 0
+  }
+  
+  return Object.values(obj)
+    .map(value => getSum(value))
+    .reduce((acc, val) => acc + val, 0)
+}
+
+const result = getSum(JSON.parse(input))
+console.log(result)
