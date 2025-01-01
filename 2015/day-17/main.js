2c9ea394ab176610f37c20d761e5513f91e39062
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-01-01 20:59:11 +0100

    2015 day 17

---
+++ 2015/day-17/main.js
@@ -0,0 +1,34 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const LITERS = 150
+
+const inputs = readFile(FILE_NAME)
+const containers = []
+const possibilities = new Set()
+
+for (let i = 0; i <= inputs.length; i++) {
+  containers.push({ id: i, value: parseInt(inputs[i]) })
+}
+
+const runPossibilities = (containers, filledContainerIds, remainingLiters) => {
+  if (remainingLiters === 0) {
+    possibilities.add(filledContainerIds.sort().join('-'))
+    return
+  }
+
+  containers.filter(container => container.value <= remainingLiters
+    && (filledContainerIds.length === 0 || container.id > filledContainerIds[filledContainerIds.length - 1]))
+    .map(container =>
+      runPossibilities(
+        containers.filter(c => c.id !== container.id),
+        [...filledContainerIds, container.id],
+        remainingLiters - container.value
+      )
+    )
+}
+
+runPossibilities(containers, [], LITERS)
+
+console.log(possibilities.size)
