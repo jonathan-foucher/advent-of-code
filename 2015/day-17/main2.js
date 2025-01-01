95489dddc96fb828115dbbac7379ccc1bf405e44
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-01 20:59:11 +0100

    2015 day 17

---
+++ 2015/day-17/main2.js
@@ -0,0 +1,42 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const LITERS = 150
+
+const inputs = readFile(FILE_NAME)
+const containers = []
+const possibilities = new Set()
+
+for (let i = 0; i <= inputs.length; i++) {
+  containers.push({ id: i, value: parseInt(inputs[i]) })
+}
+
+const runPossibilities = (containers, filledContainerIds, remainingLiters) => {
+  if (remainingLiters === 0) {
+    possibilities.add(filledContainerIds.sort().join('-'))
+    return
+  }
+
+  containers.filter(container => container.value <= remainingLiters
+    && (filledContainerIds.length === 0 || container.id > filledContainerIds[filledContainerIds.length - 1]))
+    .map(container =>
+      runPossibilities(
+        containers.filter(c => c.id !== container.id),
+        [...filledContainerIds, container.id],
+        remainingLiters - container.value
+      )
+    )
+}
+
+runPossibilities(containers, [], LITERS)
+
+const minContainers = Array.from(possibilities)
+  .map(possibility => possibility.match(/-/g).length)
+  .reduce((acc, val) => acc > val ? val : acc, possibilities.size)
+
+const result = Array.from(possibilities)
+  .filter(possibility => possibility.match(/-/g).length === minContainers)
+  .length
+
+console.log(result)
