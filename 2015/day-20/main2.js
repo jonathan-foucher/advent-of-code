943f7d0c9223509acf5eebc31d2c4184c3ed373f
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-03 00:17:14 +0100

    2015 day 20

---
+++ 2015/day-20/main2.js
@@ -0,0 +1,22 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const TARGET = parseInt(readFile(FILE_NAME)[0])
+
+let max = 0
+let n = 0
+while(max < TARGET) {
+  n++
+  let res = 10
+  for (let i = 2; i <= n; i++) {
+    if (n % i === 0 && n / i <= 50) {
+      res += 11 * i
+    }
+  }
+  if (res > max) {
+    max = res
+  }
+}
+
+console.log(n)
