0fb7e1ad4283b79caa5c7e73b011c6889cde9c5d
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-04 17:28:44 +0100

    2016 day 4

---
+++ 2016/day-04/main.js
@@ -0,0 +1,51 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const rooms = readFile(FILE_NAME)
+  .map(line => {
+    const name = /^.+(?=\-\d+)/.exec(line)[0]
+    const sectorId = parseInt(/(?<=\-)\d+(?=\[)/.exec(line)[0])
+    const checksum = /(?<=\[)[a-z]+(?=\]$)/.exec(line)[0]
+    return { name, sectorId, checksum }
+  })
+
+let result = 0
+for (let i = 0; i < rooms.length; i++) {
+  const room = rooms[i]
+
+  const checksum = room.name.split('')
+    .filter(letter => letter !== '-')
+    .reduce((acc, val) => {
+      let found = acc.find(e => e.letter === val)
+      if (found) {
+        found.count++
+      } else {
+        acc.push({ letter: val, count: 1 })
+      }
+      return acc
+    }, [])
+    .sort((a, b) => {
+      if (a.count > b.count) {
+        return -1
+      }
+      if (a.count < b.count) {
+        return 1
+      }
+
+      if (a.letter > b.letter) {
+        return 1
+      }
+
+      return -1
+    })
+    .map(e => e.letter)
+    .slice(0, 5)
+    .join('')
+
+  if (room.checksum === checksum) {
+    result += room.sectorId
+  }
+}
+
+console.log(result)
