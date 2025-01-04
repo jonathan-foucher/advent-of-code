5c23a7a2e1b1d33715437b54dbedff93d2d24337
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-04 16:30:01 +0100

    2016 day 2

---
+++ 2016/day-02/main.js
@@ -0,0 +1,42 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const inputs = readFile(FILE_NAME)
+
+const PAD_SIZE = 3
+const NUM_PAD = [
+  { value: '1', x: 0, y: 0 },
+  { value: '2', x: 1, y: 0 },
+  { value: '3', x: 2, y: 0 },
+  { value: '4', x: 0, y: 1 },
+  { value: '5', x: 1, y: 1 },
+  { value: '6', x: 2, y: 1 },
+  { value: '7', x: 0, y: 2 },
+  { value: '8', x: 1, y: 2 },
+  { value: '9', x: 2, y: 2 },
+]
+let x = 1
+let y = 1
+let result = ''
+
+for (let i = 0; i < inputs.length; i++) {
+  for (let j = 0; j < inputs[i].length; j++) {
+    const input = inputs[i][j]
+    if (input === 'U' && y > 0) {
+      y--
+    }
+    if (input === 'L' && x > 0) {
+      x--
+    }
+    if (input === 'D' && y < PAD_SIZE - 1) {
+      y++
+    }
+    if (input === 'R' && x < PAD_SIZE - 1) {
+      x++
+    }
+  }
+  result += NUM_PAD.find(key => key.x === x && key.y === y).value
+}
+
+console.log(result)
