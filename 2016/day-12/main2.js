07a7b8e54c2ab69d0df64036c3d19f5a856be576
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-01-11 23:36:26 +0100

    2016 day 12

---
+++ 2016/day-12/main2.js
@@ -0,0 +1,50 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+let registers = { a: 0, b: 0, c: 1, d: 0 }
+
+const instructions = readFile(FILE_NAME)
+  .map(line => {
+    const splitted = line.split(' ')
+    const params = [ splitted[1], splitted[2] ].filter(param => param)
+      .map(param => {
+        if (!isNaN(parseInt(param))) {
+          return parseInt(param)
+        }
+        return param
+      })
+    return { action: splitted[0], params }
+  })
+
+const PROGRAM_LENGTH = instructions.length
+
+let i = 0
+while(i < PROGRAM_LENGTH && i >= 0) {
+  const instruction = instructions[i]
+  switch (instruction.action) {
+    case 'cpy':
+    let cpy = isNaN(instruction.params[0]) ? registers[instruction.params[0]] : instruction.params[0]
+    registers[instruction.params[1]] = cpy
+    break
+
+    case 'inc':
+    registers[instruction.params[0]]++
+    break
+
+    case 'dec':
+    registers[instruction.params[0]]--
+    break
+
+    case 'jnz':
+    let value = isNaN(instruction.params[0]) ? registers[instruction.params[0]] : instruction.params[0]
+    if (value !== 0) {
+      let jumpLength = isNaN(instruction.params[1]) ? registers[instruction.params[1]] : instruction.params[1]
+      i += jumpLength - 1
+    }
+    break
+  }
+  i++
+}
+
+console.log(registers.a)
