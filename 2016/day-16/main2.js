bda45290ed9e7a469a9b747adc14ef3e04990e0d
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-01-12 14:23:14 +0100

    2016 day 16

---
+++ 2016/day-16/main2.js
@@ -0,0 +1,27 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const data = readFile(FILE_NAME)[0].split('')
+  .map(char => parseInt(char))
+const DISK_LENGTH = 35651584
+
+let length = data.length
+do {
+  data.push(0)
+  for (let i = length - 1; i >= 0; i--) {
+    data.push(1 - data[i])
+  }
+  length = data.length
+} while (length < DISK_LENGTH)
+
+let checksum = data.slice(0, DISK_LENGTH)
+while (checksum.length % 2 === 0) {
+  let temp = []
+  for (let i = 0; i < checksum.length; i += 2) {
+    temp.push(1 - Math.abs(checksum[i] - checksum[i + 1]))
+  }
+  checksum = temp
+}
+
+console.log(checksum.join(''))
