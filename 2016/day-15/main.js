8ccfb31522cb9cb2d1280c4afb885a7b3fd7ac6b
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-01-12 13:42:09 +0100

    2016 day 15

---
+++ 2016/day-15/main.js
@@ -0,0 +1,24 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const discs = readFile(FILE_NAME)
+  .map(line => {
+    const matches = [...line.match(/Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+)./)]
+    return { id: parseInt(matches[1]), nPositions: parseInt(matches[2]), startPosition: parseInt(matches[3]) }
+  })
+
+let i = -1, hasPassed = false
+while (!hasPassed) {
+  i++
+  hasPassed = true
+  for (const disc of discs) {
+    if ((disc.startPosition + disc.id + i) % disc.nPositions === 0) {
+      continue
+    }
+    hasPassed = false
+    break
+  }
+}
+
+console.log(i)
