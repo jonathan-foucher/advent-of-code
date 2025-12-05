7b1b6fcd26fbb2c4784351398f08ac34226c00a3
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-12-05 07:22:31 +0100

    2025 day 5

---
+++ 2025/day-05/main.js
@@ -0,0 +1,34 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+const RANGES = []
+const IDS = []
+
+readFile(FILE_NAME).forEach(line => {
+  if (line.includes('-')) {
+    RANGES.push([
+      parseInt(line.split('-')[0]),
+      parseInt(line.split('-')[1])
+    ])
+  } else if (line !== '') {
+    IDS.push(parseInt(line))
+  }
+  return line
+})
+
+let result = 0
+for (let i = 0; i < IDS.length; i++) {
+  const id = IDS[i]
+
+  for (let j = 0; j < RANGES.length; j++) {
+    const r0 = RANGES[j][0]
+    const r1 = RANGES[j][1]
+    if (id >= r0 && id <= r1) {
+      result++
+      break
+    }
+  }
+}
+
+console.log(result)
---
+++ 2025/day-05/main2.js
@@ -0,0 +1,35 @@
+import { readFile } from '../../utils/javascript/file-utils'
+
+const FILE_NAME = 'input/input.txt'
+
+let ranges = readFile(FILE_NAME).map(line => {
+  if (line.includes('-')) {
+    return [
+      parseInt(line.split('-')[0]),
+      parseInt(line.split('-')[1])
+    ]
+  }
+  return null
+}).filter(range => range)
+.sort((a, b) => a[0] - b[0])
+
+let mergedRanges = []
+
+for (let i = 0; i < ranges.length; i++) {
+  const r0 = ranges[i][0]
+  let r1 = ranges[i][1]
+
+  while (ranges[i + 1] && r1 + 1 >= ranges[i + 1][0]) {
+    i++
+    r1 = Math.max(r1, ranges[i][1])
+  }
+  mergedRanges.push([r0, r1])
+}
+
+let result = 0
+
+for (let i = 0; i < mergedRanges.length; i++) {
+  result += mergedRanges[i][1] - mergedRanges[i][0] + 1
+}
+
+console.log(result)
