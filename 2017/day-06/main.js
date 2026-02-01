79604699505a4e8d2ff230ae065d30061901868e
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2026-02-01 21:12:07 +0100

    2017 day 6

---
+++ 2017/day-06/main.js
@@ -0,0 +1,38 @@
+import { readFile } from '../../utils/file-utils.js'
+
+const FILE_NAME = 'input/input.txt'
+
+const banks = readFile(FILE_NAME)[0]
+  .split(/\s+/)
+  .map((str) => parseInt(str))
+
+const getNumberOfCycles = (banks, n = 0, states = []) => {
+  const currentState = banks.join('-')
+  if (states.includes(currentState)) {
+    return n
+  }
+  states.push(currentState)
+
+  let max = banks[0]
+  let maxIndex = 0
+  for (let i = 1; i < banks.length; i++) {
+    if (banks[i] > max) {
+      max = banks[i]
+      maxIndex = i
+    }
+  }
+
+  banks[maxIndex] = 0
+  let i = maxIndex
+  while (max > 0) {
+    i = (i + 1) % banks.length
+    banks[i]++
+    max--
+  }
+
+  n++
+  return getNumberOfCycles(banks, n, states)
+}
+
+const result = getNumberOfCycles(banks)
+console.log(result)
