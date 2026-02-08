7c9d4011fbaf9bd053bc35a00068ac81a2f7844b
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2026-02-08 14:46:43 +0100

    2018 day 3

---
+++ 2018/day-03/main.js
@@ -0,0 +1,29 @@
+import { readFile } from '../../utils/file-utils.js'
+
+const FILE_NAME = 'input/input.txt'
+
+const claims = readFile(FILE_NAME).map((line) => {
+  const regexResult = /#(?<id>\d+) @ (?<x>\d+),(?<y>\d+): (?<width>\d+)x(?<height>\d+)/.exec(line)
+  return {
+    id: parseInt(regexResult.groups['id']),
+    x: parseInt(regexResult.groups['x']),
+    y: parseInt(regexResult.groups['y']),
+    width: parseInt(regexResult.groups['width']),
+    height: parseInt(regexResult.groups['height']),
+  }
+})
+
+const counts = {}
+for (const claim of claims) {
+  for (let i = 0; i < claim.width; i++) {
+    for (let j = 0; j < claim.height; j++) {
+      const x = claim.x + i
+      const y = claim.y + j
+      const key = `${x}-${y}`
+      counts[key] = (counts[key] ?? 0) + 1
+    }
+  }
+}
+
+const result = Object.values(counts).filter((count) => count > 1).length
+console.log(result)
